99482627d851341a178dc79a23fa36a926c30c7f
Mathieu Desrochers <mathieu@desrochers>
2018-07-02 15:52:26 -0400

    Covered tests and doc

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,6 @@
 #[test]
 fn integration_test() {
-    // gets linked against a crate of the library
+    // gets linked against a library's crate
     // used to test the public api
     assert_eq!(1, 1);
 }
